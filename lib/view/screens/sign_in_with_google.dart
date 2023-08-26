96b5796093f68029d71749596f66cdcf85ef475f
test <test>
2023-08-26 19:26:22 +0300

    User Session and some ui.

--- lib/view/screens/sign_in_with_google.dart
+++ lib/view/screens/sign_in_with_google.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:get/get.dart';
 import 'package:link_up/constants/app_colors.dart';
 import 'package:link_up/constants/app_fonts.dart';
